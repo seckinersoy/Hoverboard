aa5d89267660a91bfd1e9858ec18b00daae632bc
Seckin Ersoy <seckinersoy06@gmail.com>
2023-04-14 17:39:06 +0300

    semicolons

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const container = document.getElementById('container')
-const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
-const SQUARES = 500
+const container = document.getElementById('container');
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+const SQUARES = 500;
 
 for (let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div');
-    square.classList.add('square')
+    square.classList.add('square');
 
-    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseover', () => setColor(square));
 
-    square.addEventListener('mouseout', () => removeColor(square))
+    square.addEventListener('mouseout', () => removeColor(square));
 
-    container.appendChild(square)
+    container.appendChild(square);
 }
 
 function setColor(element) {
-    const color = getRandomColor()
+    const color = getRandomColor();
     element.style.background = color
     element.style.boxShadow = `0 0 2px ${color}, 0  0 10px ${color}`
 }
@@ -26,5 +26,5 @@ function removeColor(element) {
 }
 
 function getRandomColor() {
-    return colors[Math.floor(Math.random() * colors.length)]
+    return colors[Math.floor(Math.random() * colors.length)];
 }
